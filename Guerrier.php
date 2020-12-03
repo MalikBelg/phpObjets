16fea2bbe6ebf08372c30ff3354d548ab523e51a
MalikBelg <malik.belguermi@gmail.com>
2020-12-03 08:18:33 +0100

    Ajout nouvelles classes

---
+++ Guerrier.php
@@ -0,0 +1,94 @@
+<?php
+
+class Guerrier {
+
+    private $nom;
+    private $force;
+    private $niveau;
+    private $sante;
+    private $mort;
+ 
+   
+
+    function __construct($nom, $force, $niveau = 1, $sante = 100){
+        $this->nom = $nom;
+        $this->force = $force;
+        $this->niveau = $niveau;
+        $this->sante = $sante;
+        $this->mort = false;
+    }
+
+
+    function caracteristiques() {
+       $etat = ($this->mort)? "mort" : "vie";
+       echo " [ " . $this->nom . " :" . " Force: " . $this->force . "; Niveau: " . $this->niveau . "; Santé: " . 
+       $this->sante . "; Etat: " . $etat . " ] </br>";
+       
+    }
+
+    function setNom(string $nomperso) {
+        $this->nom = $nomperso;
+    }
+
+    function getNom() : string {
+        return $this->nom;
+    }
+
+    function setForce(int $for) {
+        $this->force = $for;
+        return true;
+    }
+
+    function setNiveau(int $niv) {
+        $this->niveau = $niv;
+    }
+ 
+    function getNiveau(): ?int {
+        return $this->niveau;
+    }
+ 
+    function setSante(int $vie)  {
+        $this->sante = $vie;
+
+    }
+
+    function getSante(): ?int {
+        return $this->sante;
+    }    
+ 
+    function setMort(bool $mortperso) {
+        $this->mort = $mortperso;
+
+    }
+ 
+    function isMort(): ?bool {  
+        return $this->mort;
+    }
+
+    function attaquer(Guerrier $personnage){
+        
+        $this->frapper($personnage);
+    }
+
+    function frapper($perso) {
+        echo $this->nom . " frappe " . $perso->getNom() . "</br>";
+        $perso->subirDegats($this->force);
+    }
+    
+    function subirDegats(int $degats) {
+        $this->sante -= $degats;
+        if($this->sante <=0) {
+            $this->mort = true;
+        }
+    }
+
+    function levelUp(Guerrier $personnage) {
+        $this->niveau++;
+        echo $this->nom . " est niveau " . $personnage->getNiveau() . " </br>"; 
+    }
+
+    
+
+}
+// Aucun code en dehors !
+?>
--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <?php
 
 require_once "Personnage.php";
+require_once "Archer.php";
+require_once "Guerrier.php";
+require_once "Magicien.php";
 
 
-$perso1 = new Personnage("Rosa", 20, 1, 100);
-$perso2 = new Personnage("Golbu", 15, 3, 170);
-$perso3 = new Personnage("Arthis", 13, 2, 80);
+$perso1 = new Archer("Rosa", 20, 1, 100);
+$perso2 = new Guerrier("Golbu", 15, 3, 170);
+$perso3 = new Magicien("Arthis", 13, 2, 80);
 
 
 var_dump($perso1);
 var_dump($perso2);
-var_dump($perso3);
-
 
 
 $perso1->caracteristiques();
 $perso2->caracteristiques();
-$perso3->caracteristiques();
+
+$perso1->attaquer($perso2);
+
+$perso1->caracteristiques();
+$perso2->caracteristiques();
+
+
+
+
 
 
 
